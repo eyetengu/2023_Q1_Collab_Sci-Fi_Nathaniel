0911ba4209436259040f608339512b8c40fc4060
Nathaniel Townsend <cybernetikshadow@hotmail.com>
2024-03-12 21:06:38 -0500

    update player

--- Assets/_CUSTOM_ASSETS/_ABACUSAssets/Scripts/ShipPlayerController.cs
+++ Assets/_CUSTOM_ASSETS/_ABACUSAssets/Scripts/ShipPlayerController.cs
@@ -6,12 +6,12 @@ using UnityEngine.InputSystem;
 
 public class ShipPlayerController : MonoBehaviour
 {
-    public float moveSpeed;
+    public float verticalMoveSpeed;
     public InputAction inputAction;
     Vector3 moveDirection = Vector3.zero;
     Rigidbody rb;
     private Camera _camera;
-
+    public float forwardForce;
     private void OnEnable()
     {
         inputAction.Enable();
@@ -32,17 +32,17 @@ public class ShipPlayerController : MonoBehaviour
     void Update()
     {
         var inputDirection = inputAction.ReadValue<Vector2>();
-        moveDirection = new Vector3(0, inputDirection.y * moveSpeed, 0);
+        moveDirection = new Vector3(0, inputDirection.y * verticalMoveSpeed, forwardForce);
+        Vector3 screenCoordinate = _camera.WorldToScreenPoint(transform.position);
+        if ((screenCoordinate.y < 0 && moveDirection.y < 0) ||
+            (screenCoordinate.y > _camera.pixelHeight && moveDirection.y > 0))
+        {
+            moveDirection = new Vector3(moveDirection.x, 0, moveDirection.z);
+        }
     }
 
     private void FixedUpdate()
     {  
         rb.velocity = moveDirection;
-        Vector3 screenCoordinate = _camera.WorldToScreenPoint(transform.position);
-        if((screenCoordinate.y < 0 && rb.velocity.y < 0)|| 
-            (screenCoordinate.y > _camera.pixelHeight && rb.velocity.y > 0))
-        {
-            rb.velocity = new Vector3(rb.velocity.x, 0);
-        }
     }
 }
