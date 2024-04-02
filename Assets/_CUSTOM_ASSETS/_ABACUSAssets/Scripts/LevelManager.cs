584d3dc42fe70541a12fea09991a6b88996fa5d6
Nathaniel Townsend <cybernetikshadow@hotmail.com>
2024-04-01 22:41:17 -0500

    Objective Manager

--- Assets/_CUSTOM_ASSETS/_ABACUSAssets/Scripts/LevelManager.cs
+++ Assets/_CUSTOM_ASSETS/_ABACUSAssets/Scripts/LevelManager.cs
@@ -1,11 +1,80 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
-    public MissionType MissionType;
-    public DifficultyLevel difficultyLevel;
+    [SerializeField] private DifficultyLevel _difficultyLevel;
+    [SerializeField] private MissionType _missionType;
+    [SerializeField] private float _remainingMissionTime = -1;
+    [SerializeField] private bool _levelCompleted;
+    private bool _stopTimerCalled;
+
+    public MissionType MissionType { get { return _missionType; } }
+    public DifficultyLevel DifficultyLevel { get { return _difficultyLevel; } }
+    public float RemainingMissionTime { get { return _remainingMissionTime; } }
+
+    public void SetMission(MissionType missionType)
+    {
+        _missionType = missionType;
+    }
+    public void SetDifficulty(DifficultyLevel level)
+    {
+        _difficultyLevel = level;
+    }
+    public void SetLevelCompleted()
+    {
+        _levelCompleted = true;
+    }
+    public void StartMissionTimer()
+    {
+        switch (_difficultyLevel)
+        {
+            case DifficultyLevel.Easy:
+                _remainingMissionTime = Time.time + 600.0f;
+                StartCoroutine(CountDownTimer());
+                break;
+            case DifficultyLevel.Medium:
+                _remainingMissionTime = Time.time + 300.0f;
+                StartCoroutine(CountDownTimer());
+                break;
+            case DifficultyLevel.Hard:
+                _remainingMissionTime = Time.time + 300.0f;
+                StartCoroutine(CountDownTimer());
+                break;
+            case DifficultyLevel.Free:
+                StartCoroutine(CountUpTimer());
+                break;
+            default:
+                Debug.Log("Difficulty not valid");
+                break;
+        }
+    }
+    public void StopMissionTimer()
+    {
+        _stopTimerCalled = true;
+    }
+    IEnumerator CountDownTimer()
+    {
+        while (!_levelCompleted || !_stopTimerCalled)
+        {
+            _remainingMissionTime -= 1;
+            if (_remainingMissionTime < 0)
+            {
+                SetLevelCompleted();
+            }
+            yield return new WaitForSeconds(1);
+        }
+    }
+    IEnumerator CountUpTimer()
+    {
+        while (!_levelCompleted || !_stopTimerCalled)
+        {
+            _remainingMissionTime += 1;
+            yield return new WaitForSeconds(1);
+        }
+    }
 
 }
 
---
+++ Assets/_CUSTOM_ASSETS/_ABACUSAssets/Scripts/TimeUI.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine;
+using TMPro;
+
+public class TimeUI : MonoBehaviour
+{
+    [SerializeField] private LevelManager levelManager;
+    [SerializeField] private GameObject displayTextObject;
+
+    void Update()
+    {
+        if (levelManager == null)
+        {
+            Debug.LogError("TimeUI>> level manager not assigned");
+            return;
+        }
+
+        if (displayTextObject == null)
+        {
+            Debug.LogError("TimeUI>> displayTextObject not assigned");
+            return;
+        }
+
+        float remainingTime = levelManager.RemainingMissionTime;
+        float displayMinute = Mathf.Floor(remainingTime / 60f);
+        float displaySeconds = Mathf.Floor(remainingTime % 60f);
+
+        displayTextObject.GetComponent<TextMeshProUGUI>().text = string.Format("{0:00}:{1:00}", displayMinute, displaySeconds);
+    }
+}
