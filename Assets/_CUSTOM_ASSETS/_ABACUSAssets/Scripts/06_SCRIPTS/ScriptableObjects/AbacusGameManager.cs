dd8817f9e015ef4082c4bcb527e32925e10991dc
Nathaniel Townsend <cybernetikshadow@hotmail.com>
2024-10-04 00:19:15 -0500

    remove unused scenes code and scripts

--- Assets/_CUSTOM_ASSETS/_ABACUSAssets/Scripts/06_SCRIPTS/ScriptableObjects/AbacusGameManager.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "AbacusGameManager", menuName = "ScriptableObjects/AbacusGameManager", order = 1)]
-public class AbacusGameManager : ScriptableObject
-{
-    public string PlayerName { get; set; }
-}
