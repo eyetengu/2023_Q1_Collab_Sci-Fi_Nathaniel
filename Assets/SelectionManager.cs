a5127a99026a078a41c7f575320c944356895e68
Nathaniel Townsend <cybernetikshadow@hotmail.com>
2024-10-20 19:42:39 -0500

    clean up

--- Assets/SelectionManager.cs
+++ Assets/SelectionManager.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
-using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
