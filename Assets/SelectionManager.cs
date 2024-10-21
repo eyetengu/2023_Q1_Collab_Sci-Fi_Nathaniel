a3b2c6465f06c65bb0570d8ba7675bebfa04f088
Nathaniel Townsend <cybernetikshadow@hotmail.com>
2024-10-20 19:45:23 -0500

    separate classes

--- Assets/SelectionManager.cs
+++ Assets/SelectionManager.cs
@@ -55,14 +55,3 @@ public class SelectionManager : MonoBehaviour
 
     }
 }
-
-public enum SceneSelection
-{
-    MainMenu = 0,
-    ConvoyDefender,
-    RaiderSwarm1,
-    RaiderSwarm2,
-    RaiderSwarm3,
-    RaiderSwarmCredits,
-    MainMenuCredits
-}
